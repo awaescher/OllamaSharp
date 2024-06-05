6f28381ed2594ec2639c1bda589aef4c2ffde88f
JD Davis <mxjerrett@gmail.com>
2024-06-04 19:45:18 -0500

    fix: updated GenerateEmbeddingRequest to utilize RequestOptions to properly pass request parameters.

--- src/Models/GenerateEmbedding.cs
+++ src/Models/GenerateEmbedding.cs
@@ -7,18 +7,42 @@ namespace OllamaSharp.Models;
 /// </summary>
 public class GenerateEmbeddingRequest
 {
+	/// <summary>
+	/// The name of the model to generate embeddings from
+	/// </summary>
 	[JsonPropertyName("model")]
-	public string Model { get; set; }
+	public string Model { get; set; } = null!;
 
+	/// <summary>
+	/// The text to generate embeddings for
+	/// </summary>
 	[JsonPropertyName("prompt")]
-	public string Prompt { get; set; }
+	public string Prompt { get; set; } = null!;
 
+	/// <summary>
+	/// Additional model parameters listed in the documentation for the Modelfile
+	/// such as temperature.
+	/// </summary>
 	[JsonPropertyName("options")]
-	public string Options { get; set; }
+	public RequestOptions? Options { get; set; }
+	
+	/// <summary>
+	/// Gets or sets the KeepAlive property, which decides how long a given
+	/// model should stay loaded.
+	/// </summary>
+	[JsonPropertyName("keep_alive")]
+	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+	public string? KeepAlive { get; set; }
 }
 
+/// <summary>
+/// The response from the /api/embeddings endpoint
+/// </summary>
 public class GenerateEmbeddingResponse
 {
+	/// <summary>
+	/// An array of embeddings for the input text
+	/// </summary>
 	[JsonPropertyName("embedding")]
-	public double[] Embedding { get; set; }
+	public double[] Embedding { get; set; } = null!;
 }
