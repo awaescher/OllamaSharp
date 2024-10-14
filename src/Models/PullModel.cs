ab61de1f5e3db42b13ba0c9a5f18c310148e37f8
Andreas Wäscher <awaescher@gmx.de>
2024-10-14 08:43:38 +0200

    Add xml documentation
    #101

--- src/Models/PullModel.cs
+++ src/Models/PullModel.cs
@@ -3,48 +3,49 @@ using System.Text.Json.Serialization;
 namespace OllamaSharp.Models;
 
 /// <summary>
-/// https://github.com/jmorganca/ollama/blob/main/docs/api.md#pull-a-model
+/// Represents a request to pull a model from the API.
+/// <see href="https://github.com/jmorganca/ollama/blob/main/docs/api.md#pull-a-model"/>
 /// </summary>
 public class PullModelRequest : OllamaRequest
 {
 	/// <summary>
-	/// The name of the model to pull in the form of <namespace>/<model>:<tag>
+	/// Gets or sets the name of the model to pull.
 	/// </summary>
 	[JsonPropertyName("model")]
 	public string? Model { get; set; }
 }
 
 /// <summary>
-/// The streamed response from the /api/pull endpoint
+/// Represents the streamed response from the /api/pull endpoint.
 /// </summary>
 public class PullModelResponse
 {
 	/// <summary>
-	/// The status of the pull operation
+	/// Gets or sets the status of the pull operation.
 	/// </summary>
 	[JsonPropertyName("status")]
 	public string Status { get; set; } = null!;
 
 	/// <summary>
-	/// The hash of the model file
+	/// Gets or sets the hash of the model file.
 	/// </summary>
 	[JsonPropertyName("digest")]
 	public string Digest { get; set; } = null!;
 
 	/// <summary>
-	/// The total number of bytes to pull
+	/// Gets or sets the total number of bytes to pull.
 	/// </summary>
 	[JsonPropertyName("total")]
 	public long Total { get; set; }
 
 	/// <summary>
-	/// The number of bytes pulled so far
+	/// Gets or sets the number of bytes pulled so far.
 	/// </summary>
 	[JsonPropertyName("completed")]
 	public long Completed { get; set; }
 
 	/// <summary>
-	/// The percentage of the pull operation that has been completed
+	/// Gets the percentage of the pull operation that has been completed.
 	/// </summary>
 	[JsonIgnore]
 	public double Percent => Total == 0 ? 100.0 : Completed * 100.0 / Total;
--- src/Models/PushModel.cs
+++ src/Models/PushModel.cs
@@ -3,48 +3,49 @@ using System.Text.Json.Serialization;
 namespace OllamaSharp.Models;
 
 /// <summary>
-/// https://github.com/jmorganca/ollama/blob/main/docs/api.md#push-a-model
+/// Represents a request to push a model.
 /// </summary>
 public class PushModelRequest : OllamaRequest
 {
 	/// <summary>
-	/// Name of the model to push in the form of <namespace>/<model>:<tag>
+	/// Gets or sets the name of the model to push in the form of namespace/model:tag.
 	/// </summary>
 	[JsonPropertyName("model")]
 	public string? Model { get; set; }
 
 	/// <summary>
-	/// Allow insecure connections to the library. Only use this if you are pulling from your own library during development.
+	/// Gets or sets a value indicating whether to allow insecure connections to the library.
+	/// Only use this if you are pulling from your own library during development.
 	/// </summary>
 	[JsonPropertyName("insecure")]
 	public bool Insecure { get; set; }
 
 	/// <summary>
-	/// Whether to stream the response
+	/// Gets or sets a value indicating whether to stream the response.
 	/// </summary>
 	[JsonPropertyName("stream")]
 	public bool Stream { get; set; }
 }
 
 /// <summary>
-/// The response from the /api/push endpoint
+/// Represents the response from the /api/push endpoint.
 /// </summary>
 public class PushModelResponse
 {
 	/// <summary>
-	/// The status of the push operation
+	/// Gets or sets the status of the push operation.
 	/// </summary>
 	[JsonPropertyName("status")]
 	public string Status { get; set; } = null!;
 
 	/// <summary>
-	/// The hash of the model file
+	/// Gets or sets the hash of the model file.
 	/// </summary>
 	[JsonPropertyName("digest")]
 	public string Digest { get; set; } = null!;
 
 	/// <summary>
-	/// The total number of bytes to push
+	/// Gets or sets the total number of bytes to push.
 	/// </summary>
 	[JsonPropertyName("total")]
 	public int Total { get; set; }
