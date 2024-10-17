5b568ae1e0a8829cb8e13a92f5b11502585709e8
awaescher <awaescher@gmx.de>
2024-10-17 13:29:04 +0200

    Improve OllamaChatClient constructor overloads

--- src/OllamaSharp.Microsoft.Extensions.AI/OllamaChatClient.cs
+++ src/OllamaSharp.Microsoft.Extensions.AI/OllamaChatClient.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,15 +15,64 @@ namespace OllamaSharp;
 /// The Ollama implementation for Microsoft.Extensions.AI
 /// </summary>
 /// <param name="apiClient">The api client used to communicate with the Ollama endpoint</param>
-public class OllamaChatClient(IOllamaApiClient apiClient) : IChatClient
+public class OllamaChatClient : IChatClient
 {
 	/// <summary>
 	/// Gets the api client used to communicate with the Ollama endpoint
 	/// </summary>
-	public IOllamaApiClient ApiClient { get; } = apiClient ?? throw new ArgumentNullException(nameof(apiClient));
+	public IOllamaApiClient ApiClient { get; }
 
 	/// <inheritdoc/>
 	public ChatClientMetadata Metadata => new("ollama", ApiClient.Uri, ApiClient.SelectedModel);
+
+	/// <summary>
+	/// Creates a new instance of the Ollama implementation of IChatClient
+	/// </summary>
+	/// <param name="uriString">The uri of the Ollama API endpoint</param>
+	/// <param name="defaultModel">The default model that should be used with Ollama</param>
+	public OllamaChatClient(string uriString, string defaultModel = "")
+		: this(new Uri(uriString), defaultModel)
+	{
+	}
+
+	/// <summary>
+	/// Creates a new instance of the Ollama implementation of IChatClient
+	/// </summary>
+	/// <param name="uri">The uri of the Ollama API endpoint</param>
+	/// <param name="defaultModel">The default model that should be used with Ollama</param>
+	public OllamaChatClient(Uri uri, string defaultModel = "")
+		: this(new OllamaApiClient.Configuration { Uri = uri, Model = defaultModel })
+	{
+	}
+
+	/// <summary>
+	/// Creates a new instance of the Ollama implementation of IChatClient
+	/// </summary>
+	/// <param name="config">The configuration for the Ollama API client</param>
+	public OllamaChatClient(OllamaApiClient.Configuration config)
+		: this(new HttpClient() { BaseAddress = config.Uri }, config.Model)
+	{
+	}
+
+	/// <summary>
+	/// Creates a new instance of the Ollama implementation of IChatClient
+	/// </summary>
+	/// <param name="client">The Http client to access the Ollama API with</param>
+	/// <param name="defaultModel">The default model that should be used with Ollama</param>
+	/// <exception cref="ArgumentNullException"></exception>
+	public OllamaChatClient(HttpClient client, string defaultModel = "")
+		: this(new OllamaApiClient(client, defaultModel))
+	{
+	}
+
+	/// <summary>
+	/// Creates a new instance of the Ollama implementation of IChatClient
+	/// </summary>
+	/// <param name="apiClient">The api client used to communicate with the Ollama endpoint</param>
+	public OllamaChatClient(IOllamaApiClient apiClient)
+	{
+		ApiClient = apiClient ?? throw new ArgumentNullException(nameof(apiClient));
+	}
 
 	/// <inheritdoc/>
 	public async Task<ChatCompletion> CompleteAsync(IList<ChatMessage> chatMessages, ChatOptions? options = null, CancellationToken cancellationToken = default)
