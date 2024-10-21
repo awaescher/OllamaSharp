555da6e537473e03941da37cf4dc2eaa0ac7cde2
awaescher <awaescher@gmx.de>
2024-10-21 21:47:05 +0200

    Remove unused usings

--- src/Models/Chat/Converter/StringDictionaryConverter.cs
+++ src/Models/Chat/Converter/StringDictionaryConverter.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Text.Json.Serialization;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace OllamaSharp.Models.Chat.Converter;
 
