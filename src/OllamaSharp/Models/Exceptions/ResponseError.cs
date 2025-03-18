b1d25ca1be5e066aadad019652be338f435402ad
EAD\ghorner2 <ghorner2@dxc.com>
2025-03-18 00:40:36 +0000

    update to detect a error response

---
+++ src/OllamaSharp/Models/Exceptions/ResponseError.cs
@@ -0,0 +1,13 @@
+﻿namespace OllamaSharp.Models.Exceptions;
+
+/// <summary>
+/// Represents an exception thrown when a response is a ErrorResponse.
+/// </summary>
+public class ResponseError : OllamaException
+{
+	/// <summary>
+	/// </summary>
+	/// <param name="message"></param>
+	public ResponseError(string message) : base(message)
+	{ }
+}
