f0a9fa4dacfb237e7d7b04ad8dfaa6fed97f6b86
Andreas Wäscher <awaescher@gmx.de>
2025-09-11 13:36:36 +0200

    Add missing namespace

--- src/OllamaSharp/OllamaApiClient.cs
+++ src/OllamaSharp/OllamaApiClient.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 using Microsoft.Extensions.AI;
 using OllamaSharp.Constants;
 using OllamaSharp.MicrosoftAi;
